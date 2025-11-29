ca24b9fe6b0937d05a603aeb960d363f3ade9214
AMAN JAISWAL <2k23.cs2313633@gmail.com>
2025-11-29 19:21:14 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,36 @@
 const { getUser } = require("../service/auth");
+
 function checkForAuthentication(req, res, next) {
     const authorizationHeaderValue = req.headers["authorization"];
-req.user = null;
+    req.user = null;
 
+    if (
+        !authorizationHeaderValue ||
+        !authorizationHeaderValue.startsWith("Bearer")
+    ) {
+        return next();
+    }
 
-    if(
-  !authorizationHeaderValue ||
-  !authorizationHeaderValue.startWith("Bearer")
-)
-return next();
+    const token = authorizationHeaderValue.split("Bearer ")[1];
+    const user = getUser(token);
 
-const token  = authorizationHeaderValue.split("Bearer")[1];
-const user = getUser(token);
-req.user = user;
-return next();
-
+    req.user = user;
+    return next();
 }
-//ADMIN, NORMA
-function restrictTo(roles) {
-return function(req, res, next) {
-    if(!req.user) return res.redirect("/login");
-    if(!roles.includes(req.user.role))
-};
+
+function restrictTo(roles = []) {
+    return function(req, res, next) {
+        if (!req.user) return res.redirect("/login");
+
+        if (!roles.includes(req.user.role)) {
+            return res.end("UnAuthorized"); 
+        }
+
+        return next(); 
+    };
 }
 
 module.exports = {
- checkForAuthentication,
+    checkForAuthentication,
+    restrictTo, 
 };
